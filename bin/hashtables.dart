88884bbe3789605e8b7be448c667276aba1dbe9b
Ellie068 <evixosity@gmail.com>
2025-10-09 11:37:22 +0800

    created filler stuff of 3 mil value slot

--- bin/hashtables.dart
+++ bin/hashtables.dart
@@ -4,6 +4,8 @@ import 'package:hashtables/hash_search.dart';
 
 void main() {
   final myList = [42, 3, 10, 7, 28, 149, 8, 3, 25, 7, 1];
+  var foundIndex = linearSearch(myList, 28);
+  print(foundIndex);
 
   myList.sort();
   print(myList);
@@ -12,26 +14,30 @@ void main() {
   final target = binarySearch(myList, 42);
   print(target);
 
-  final studentList = [
-    Student(2024078, 'AZ1'),
-    Student(2024077, 'AZ2'),
-    Student(2021018, 'AD'),
-    Student(2023139, 'BB'),
-    Student(2025068, 'BS'),
-    Student(2024008, 'DD'),
-    Student(2024095, 'ES1'),
-    Student(2024012, 'ED1'),
-    Student(2024054, 'EB'),
-    Student(2024081, 'ED2'),
-    Student(2024111, 'ES2'),
-    Student(2024059, 'MI'),
-    Student(2024025, 'MB'),
-    Student(2024020, 'MC'),
-    Student(2023002, 'NM'),
-    Student(2022081, 'TG'),
-    Student(2024085, 'TK'),
-    Student(2023096, 'TA'),
-    Student(2022007, 'UM'),
-    Student(2024007, 'VG'),
-  ]
+  final studentList = List<Student?>.filled(3000000, null);
+  // final studentList = [
+  //   Student(2024078, 'AZ1'),
+  //   Student(2024077, 'AZ2'),
+  //   Student(2021018, 'AD'),
+  //   Student(2023139, 'BB'),
+  //   Student(2025068, 'BS'),
+  //   Student(2024008, 'DD'),
+  //   Student(2024095, 'ES1'),
+  //   Student(2024012, 'ED1'),
+  //   Student(2024054, 'EB'),
+  //   Student(2024081, 'ED2'),
+  //   Student(2024111, 'ES2'),
+  //   Student(2024059, 'MI'),
+  //   Student(2024025, 'MB'),
+  //   Student(2024020, 'MC'),
+  //   Student(2023002, 'NM'),
+  //   Student(2022081, 'TG'),
+  //   Student(2024085, 'TK'),
+  //   Student(2023096, 'TA'),
+  //   Student(2022007, 'UM'),
+  //   Student(2024007, 'VG'),
+  // ];
+
+  final studentName = studentSearch(studentList, 2024080);
+  print(studentName);
 }
